56229842b5e416faac6936e415713304a4607be0
Desiah Barnett <desiahbarnett@gmail.com>
2024-05-17 21:21:50 -0400

    app complete

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,21 +1,18 @@
-import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
+// Resume.js
+import React from 'react';
 
 
-
-function Footer() {
+function Resume() {
   return (
-    <footer>
-      <div className="social-icons">
-        <a href="https://www.linkedin.com/in/desiah-barnett" target="_blank" rel="noopener noreferrer"><img src="https://upload.wikimedia.org/wikipedia/commons/c/c9/Linkedin.svg" alt="LinkedIn" /></a>
-        <a href="https://github.com/DesiahBarnett" target="_blank" rel="noopener noreferrer"><img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub" /></a>
-        <a href="https://twitter.com/desiahbarnett" target="_blank" rel="noopener noreferrer"><img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Twitter_logo.svg" alt="Twitter" /></a>
-        <a href="https://drive.google.com/file/d/15eo4LSfzTdI9a-4h4d67PdtH1L7cnjJY/view?usp=drive_link" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFilePdf} /></a>
+    <div className="resume-container">
+      <h1 className="resume-header">Thanks for connecting</h1>
+      <img className="resume-image" src="https://res.cloudinary.com/doyp4tk82/image/upload/v1715989530/IMG_1497_scbrk3.jpg" alt="Your Image" />
+      <div>
+        <a href="https://drive.google.com/file/d/15eo4LSfzTdI9a-4h4d67PdtH1L7cnjJY/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+          <button className="resume-button">Resume</button>
+        </a>
       </div>
- 
-      <p>&copy; 2024 Desiah Barnett</p>
-    </footer>
+    </div>
   );
 }
 
