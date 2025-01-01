551a641e648560e717a46df2abff94e09aa38f10
Desiah Barnett <desiahbarnett@gmail.com>
2024-12-31 21:17:29 -0500

    Fix port configuration for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
-const Message = require('./models/Message');  // Import the Mongoose model
+const Message = require('/models/Message');  // Import the Mongoose model
 
 const app = express();
 
